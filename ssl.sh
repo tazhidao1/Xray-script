967f0c7c0f41e310ce898db5f94bd7e16b265488
tazhidao1 <example.com@example.com>
2025-11-18 00:14:01 +0800

    update

--- ssl.sh
+++ ssl.sh
@@ -121,8 +121,7 @@ EOF
       --accountkeylength ec-256 \
       --server zerossl \
       --ocsp \
-      --debug \
-      --force
+      --debug
     # 恢复原始配置
     mv -f /usr/local/nginx/conf/nginx.conf.bak /usr/local/nginx/conf/nginx.conf
     print_error "ECC 证书申请失败。"
